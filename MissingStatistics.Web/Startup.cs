7d522296b3321e1f277925fd3b7d5b6f3b2f90c1
huxiaofeng <xiaofenghu1990@126.com>
2017-06-22 14:02:33 +0800

    新建项目

---
+++ MissingStatistics.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MissingStatistics.Web.Startup))]
+namespace MissingStatistics.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
